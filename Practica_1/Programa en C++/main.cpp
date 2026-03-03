c9af7ace4fcf532278a79decca0e9907a506ed64
IAtevoy <zs24013170@estudiantes.uv.mx>
2026-03-02 19:01:42 -0600

    Creación de Carpeta y Programa en C++

---
+++ Practica_1/Programa en C++/main.cpp
@@ -0,0 +1,15 @@
+//Programa: Calculadora de suma
+//Autor: Equipo 1 SD
+//Versión: 1.0.0
+//Descripción: Es una claculadora la cual solo realiza la operación de una suma ya establecida
+
+#include<stdio.h>
+
+int main(){
+int suma;
+float N1, N2;
+N1 = 10;
+N2 = 3;
+suma = N1 - N2;
+printf("La suma es: %d", suma);
+}
