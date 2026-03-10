28944f01a5cf24b1d789725c1c354dc243a207ab
IAtevoy <zs24013170@estudiantes.uv.mx>
2026-03-09 18:57:32 -0600

    Se actualizo la v1.0.1 PY a v1.1.0 PY
    Se agrego la entrada de datos por teclado

---
+++ Practica_1/Programa en PY/Calculadora v1.1.0 PY.py
@@ -0,0 +1,8 @@
+#Programa: Calculadora de suma
+#Autor: Equipo 1 SD
+#Versión: 1.1.0
+#Descripción: Se agrega entrada de datos por teclado.
+N1=float(input("Ingresa el dato 1: "))
+N2=float(input("Ingresa el dato 2: "))
+suma = N1 + N2
+print("La suma es:", suma)
