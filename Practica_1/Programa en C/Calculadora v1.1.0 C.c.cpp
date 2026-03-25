ce1a9e1cf3869c6207e8c102b8f331439892cdf0
Th3PapaY0ch1S08 <zS24013131@estudiantes.uv.mx>
2026-03-24 21:01:33 -0600

    Update Calculadora v1.1.0 C.c.cpp

--- Practica_1/Programa en C/Calculadora v1.1.0 C.c.cpp
+++ Practica_1/Programa en C/Calculadora v1.1.0 C.c.cpp
@@ -1,19 +1,22 @@
+//Programa: Calculadora de suma
+//Autor: Equipo 1 SD
+//Versión: 1.1.0
+//Descripción: Se agrega entrada de datos por teclado.
+
 #include<stdio.h>
 
 int main(){
     float suma;
     float N1, N2;
-    // Quitamos "float input;" porque no se usa como variable, sino como función
 
     printf("Ingresa el dato 1: ");
-    scanf("%f", &N1); // Quitamos 'float' de aquí
+    scanf("%f", &N1);
 
     printf("Ingresa el dato 2: ");
-    scanf("%f", &N2); // Quitamos 'float' de aquí
+    scanf("%f", &N2);
 
-    suma = N1 + N2;   // Quitamos 'float' de aquí y corregimos el N2N2
+    suma = N1 + N2;
 
-    printf("La suma es: %.2f", suma); // Cambiamos print por printf
-
+    printf("La suma es: %.2f", suma); /
     return 0;
 }
